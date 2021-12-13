9fb363bf3ef0a295109d8122f3b133c6bb8a0809
SFPmedia <s-pauck@hotmail.com>
2021-12-13 10:09:29 +0100

    Update with example for array of objects

--- template-json/javascript.js
+++ template-json/javascript.js
@@ -1,14 +1,23 @@
 var returnData = () => {
   const data = require("./data.json");
+  const dataArray = require("./data-array.json");
   const persons = data.persons;
   const theOthers = data.someoneElse[1].Name;
 
   console.log(theOthers);
+  console.log(dataArray);
 
   for (let iKey of Object.keys(data)) {
     var j = data[iKey];
     console.log(iKey, j);
   }
+
+  dataArray.forEach((obj) => {
+    Object.entries(obj).forEach(([key, value]) => {
+      console.log(`${key} ${value}`);
+    });
+    console.log(obj);
+  });
 };
 
 return returnData();
