4278d527c17617b889f7ba18e81ebded172ba965
SFPmedia <s-pauck@hotmail.com>
2021-12-16 14:51:16 +0100

    Added comments

--- template-redux-react/src/index.js
+++ template-redux-react/src/index.js
@@ -8,9 +8,13 @@ import { createStore, applyMiddleware } from "redux";
 import reducer from "./reducer";
 import thunk from "redux-thunk";
 
+// The data presented in the view is stored in the Store
+// Thunk empowers action creators to decide when and what action to dispatch (great for async actions)
+// For complex applications, it is advisable to build a separate layer to manage the asynchronous operations
 const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
+  // Provider gives access to the store for the heirarchy below (<App />)
   <Provider store={store}>
     <App />
   </Provider>,
--- template-redux-react/src/reducer.js
+++ template-redux-react/src/reducer.js
@@ -2,6 +2,8 @@ const initialState = {
   todos: [],
 };
 
+// Remember that reducers have to be pure functions, in order to achieve state predictability
+// Do not include API calls, usage tracking metrics and other actions that could introduce side effects
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "SET_TODOS":
